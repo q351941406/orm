1a455f21c67d6b56e6b1bae25c3396ec3426d099
sadfsdf <351941406@qq.com>
2022-11-14 03:40:26 +0800

    修改频率限制

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:100000,1',
+            'throttle:10000000,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
