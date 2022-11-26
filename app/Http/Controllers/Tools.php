810bb4ad93b1d7c8c5cbfc34c1358a7120a8a5e1
sadfsdf <351941406@qq.com>
2022-11-26 17:00:31 +0800

    no message

--- app/Http/Controllers/Tools.php
+++ app/Http/Controllers/Tools.php
@@ -7,8 +7,9 @@ class Tools {
         $contents = file_get_contents('/proc/meminfo');
         preg_match_all('/(\w+):\s+(\d+)\s/', $contents, $matches);
         $info = array_combine($matches[1], $matches[2]);
-        dd($info);
-        return $info['MemFree']/1024/1204;
+        $MemFree = $info['MemFree']/1024/1204;
+        $MemAvailable = $info['MemAvailable']/1024/1204;
+        return $MemAvailable;
     }
 }
 
